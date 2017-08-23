7bd19968067068ef361c269ceded513b7f46966b
Pedro Goncalves <pedro.silva@webedia-group.com>
2017-08-23 10:17:21 -0300

    add stuff

--- app.js
+++ app.js
@@ -7,6 +7,33 @@ window.App = {
 
   on: function(selector) {
     return this.element.querySelector(selector);
+  },
+
+  crel: params => {
+    let el, a;
+    el = document.createElement(params.tag);
+
+    params.className ? (el.className = params.className) : null;
+
+    if (params.attributes) {
+      for (a in params.attributes) {
+        el.setAttribute(a, params.attributes[a]);
+      }
+    }
+
+    params.text && el.appendChild(document.createTextNode(params.text));
+
+    if (params.children && params.children.length) {
+      for (i = 0; i < params.children.length; i++) {
+        el.appendChild(
+          params.children[i] instanceof window.HTMLElement
+            ? params.children[i]
+            : App.crel(params.children[i])
+        );
+      }
+    }
+
+    return el;
   }
 };
 
@@ -49,7 +76,8 @@ window.SearchForm = {
   },
 
   buildResultElement: function(result) {
-    let el = document.createElement("div");
+    let el = document.createElement("a");
+    el.setAttribute("href", `anime.html?id=${result.id}`);
     el.classList.add("anime-item");
 
     let title = document.createElement("div");
@@ -69,6 +97,79 @@ window.SearchForm = {
     /* el.appendChild(synopsis); */
 
     return el;
+  }
+};
+
+window.AnimePage = {
+  getAnimeDetails: () => {
+    console.log("working");
+    AnimeAPI.getAnime(window.location.search.split("?")[1].split("id=")[1]).then(res => {
+      AnimePage.buildAnimePage(res);
+    });
+  },
+
+  buildAnimePage: obj => {
+    console.log(obj.data.attributes);
+
+    let aninfo = obj.data.attributes;
+    let relationships = obj.data.relationships;
+
+    document.getElementById(
+      "anime-results"
+    ).style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9) ), url(${aninfo
+      .coverImage.original})`;
+    document.getElementById("anime-results").style.backgroundRepeat = "no-repeat";
+    document.getElementById("anime-results").style.backgroundSize = "cover";
+
+    App.on("#anime-results").appendChild(
+      App.crel({
+        tag: "img",
+        className: "hellomoto",
+        attributes: {
+          src: aninfo.posterImage.medium
+        }
+      })
+    );
+
+    App.on("#anime-results").appendChild(
+      App.crel({
+        tag: "div",
+        className: "anime-info-container",
+        attributes: {
+          id: "idzin",
+          "data-truc": "datazin"
+        },
+        children: [
+          {
+            tag: "h2",
+            className: "anime-info-title",
+            text: aninfo.canonicalTitle
+          },
+          {
+            tag: "h3",
+            className: "anime-info-releasedate",
+            text: "Released in: ",
+            children: [
+              {
+                tag: "span",
+                className: "anime-info-releasedateyear",
+                text: aninfo.startDate.substring(0, 4)
+              }
+            ]
+          },
+          {
+            tag: "h4",
+            className: "anime-info-synopsis",
+            text: aninfo.synopsis
+          },
+          {
+            tag: "h4",
+            className: "anime-info-popularity",
+            text: "Popularity: #" + aninfo.popularityRank
+          }
+        ]
+      })
+    );
   }
 };
 

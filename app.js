c35ce491d5be10678cfbe39a55f4f791176bbe7e
pgsil <Pedroion@gmail.com>
2017-09-09 11:34:56 -0300

    Fixed issues

--- app.js
+++ app.js
@@ -1,197 +1,209 @@
 window.App = {
   element: null,
 
-  init: function() {
-    this.element = document.getElementById("app");
+  /* sets App.element to .app */
+  /* runs SearchForm.init() */
+  init() {
+    this.element = document.getElementsByClassName('app')[0];
     SearchForm.init();
   },
 
-  on: function(selector) {
+  /* shortcut for .app.querySelector() */
+  on(selector) {
     return this.element.querySelector(selector);
   },
 
-  crel: params => {
-    let el, a;
-    el = document.createElement(params.tag);
-
-    params.className ? (el.className = params.className) : null;
-
-    if (params.attributes) {
-      for (a in params.attributes) {
-        el.setAttribute(a, params.attributes[a]);
-      }
-    }
-
-    params.text && el.appendChild(document.createTextNode(params.text));
-
-    if (params.children && params.children.length) {
-      for (i = 0; i < params.children.length; i++) {
-        el.appendChild(
-          params.children[i] instanceof window.HTMLElement
-            ? params.children[i]
-            : App.crel(params.children[i])
-        );
-      }
-    }
-
-    return el;
-  }
+  /* handy create element function, works with recursion */
+  
 };
 
 window.AnimeAPI = {
-  endpoint: "https://kitsu.io/api/edge",
+  endpoint: 'https://kitsu.io/api/edge',
 
-  getAnime: function(data) {
+  getAnime(data) {
     return fetch(`${this.endpoint}/anime/${data}`).then(resp => resp.json());
   },
 
-  searchAnime: function(str) {
-    return fetch(this.endpoint + "/anime?filter[text]=" + str).then(res => res.json());
-  }
+  searchAnime(str) {
+    return fetch(`${this.endpoint  }/anime?filter[text]=${  str}`).then(res => res.json());
+  },
 };
 
 window.SearchForm = {
-  targetSelector: "#animesearch",
+  targetSelector: '.animesearch',
   debounce: null,
+  loadingSpinner: document.getElementsByClassName("loading-spinner")[0],
+  element: null,
 
-  init: function(){
+  /* sets SearchForm.element to .animesearch */
+  /* adds an EventListener to keyup on the search box,
+      which runs SearchForm.handle() on key up */
+  init() {
     this.element = App.on(this.targetSelector);
 
-    this.element.addEventListener('keyup', this.handle);
+    /* if this.element, set the eventListener */
+    this.element && this.element.addEventListener('keyup', this.handle);
   },
 
-  handle: function() {
-    let value = SearchForm.element.value;
-    if (this.debounce) clearTimeout(this.debounce);
+  /* search box logic */
+  handle() {
+    const value = SearchForm.element.value;
+    if (this.debounce) clearTimeout(this.debounce);    
 
     if (value.length > 2) {
-      this.debounce = setTimeout(function() {
+      this.debounce = setTimeout(() => {
+        /* runs AnimeAPI.searchAnime(), getting results
+            from the API */
         AnimeAPI.searchAnime(value).then(resp => {
           SearchForm.buildResults(resp.data);
         });
+        /* shows the loading spinner */
+        SearchForm.loadingSpinner.classList.add("opacityone");
       }, 500);
     }
   },
 
-  buildResults: function(results) {
-    var result = App.on("#search-results")
-    result.innerHTML = "";
+  buildNoResultsFound(){
+    let result = App.on('.search-results');
+    let el = Helper.crel({ tag: 'h2', text: "No results found!" });
+    result.appendChild(el);
+  },
 
-    var logo = document.getElementById("body-logo");
+  /* renders the search result elements,
+      but first removes any results that might be there */
+  buildResults(results) {
+    let result = App.on('.search-results');
+    result.innerHTML = '';
+
+    let logo = document.getElementsByClassName('body-logo')[0];
 
     if (logo) logo.remove();
 
     for (i = 0; i < results.length; i++) {
-      let el = this.buildResultElement(results[i]);
+      const el = this.buildResultElement(results[i]);
 
       result.appendChild(el);
     }
+    if(results.length === 0){
+      this.buildNoResultsFound();
+    }
+
+    /* hides the loading spinner */
+    SearchForm.loadingSpinner.classList.remove("opacityone");
   },
 
-  buildResultElement: function(result) {
-    let el = App.crel({tag: "a"});
-    el.setAttribute("href", `anime.html?id=${result.id}`);
-    el.classList.add("anime-item");
+  /* uses crel to create the result elements themselves */
+  buildResultElement(result) {
+    const el = Helper.crel({ tag: 'a' });
+    el.setAttribute('href', `anime.html?id=${result.id}`);
+    el.classList.add('anime-item');
     el.style.backgroundImage = `url(${result.attributes.posterImage.large})`;
 
-    let title = App.crel({tag: "div"});
+    const title = Helper.crel({ tag: 'div' });
     title.innerText = result.attributes.canonicalTitle;
-    title.classList.add("anime-title");
+    title.classList.add('anime-title');
 
     el.appendChild(title);
 
     return el;
-  }
+  },
 };
 
 window.AnimePage = {
+  /* uses AnimeAPI.getAnime to get info about an anime by ID */
   getAnimeDetails: () => {
-    AnimeAPI.getAnime(window.location.search.split("?")[1].split("id=")[1]).then(res => {
+    AnimeAPI.getAnime(window.location.search.split('?')[1].split('id=')[1]).then((res) => {
       AnimePage.buildAnimePage(res);
     });
   },
 
-  show: function() {
-    let el = document.getElementsByClassName("anime-page")[0];
-    el.classList.add("active");
+  show() {
+    const el = document.getElementsByClassName('anime-page')[0];
+    el.classList.add('active');
   },
 
-  buildAnimePage: obj => {
+  /* renders the elements on the anime page using
+      the info retrieved from the API */
+  buildAnimePage: (obj) => {
     const aninfo = obj.data.attributes;
     const relationships = obj.data.relationships;
 
-    let el = document.getElementsByClassName("anime-page")[0];
-    
-    el.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9) ), url(${aninfo.coverImage ? aninfo.coverImage.original : aninfo.posterImage.original})`;
-    el.style.backgroundRepeat = "no-repeat";
-    el.style.backgroundSize = "cover";
+    const el = document.getElementsByClassName('anime-page')[0];
 
-    let img = App.crel({tag: 'img'});
+    const img = Helper.crel({ tag: 'img' });
     img.setAttribute('src', aninfo.coverImage ? aninfo.coverImage.original : aninfo.posterImage.original);
-    img.addEventListener('load', function () {
-      AnimePage.show()
+    img.addEventListener('load', () => {
+      AnimePage.show();
     });
 
-    App.on(".anime-page").appendChild(
-      App.crel({
-        tag: "img",
-        className: "anime-poster",
+    App.on('.anime-page').appendChild(
+      Helper.crel({
+        tag: 'img',
+        className: 'anime-poster',
         attributes: {
-          src: aninfo.posterImage.medium
-        }
-      })
+          src: aninfo.posterImage.medium,
+        },
+      }),
     );
 
-    App.on(".anime-page").appendChild(
-      App.crel({
-        tag: "div",
-        className: "anime-info-container",
+    /* good example on how to use crel */
+    App.on('.anime-page').appendChild(
+      Helper.crel({
+        tag: 'div',
+        className: 'anime-info-container',
         attributes: {
-          id: "idzin",
-          "data-truc": "datazin"
+          id: 'idzin',
+          'data-truc': 'datazin',
         },
         children: [
           {
-            tag: "h2",
-            className: "anime-info-title",
-            text: aninfo.canonicalTitle
+            tag: 'h2',
+            className: 'anime-info-title',
+            text: aninfo.canonicalTitle,
           },
           {
-            tag: "h3",
-            className: "anime-info-releasedate",
-            text: "Released in: ",
+            tag: 'h3',
+            className: 'anime-info-releasedate',
+            text: 'Released in: ',
             children: [
               {
-                tag: "span",
-                className: "anime-info-releasedateyear",
-                text: aninfo.startDate.substring(0, 4)
-              }
-            ]
+                tag: 'span',
+                className: 'anime-info-releasedateyear',
+                text: aninfo.startDate.substring(0, 4),
+              },
+            ],
           },
           {
-            tag: "h4",
-            className: "anime-info-synopsis",
-            text: aninfo.synopsis
+            tag: 'h4',
+            className: 'anime-info-synopsis',
+            text: aninfo.synopsis,
           },
           {
-            tag: "h4",
-            className: "anime-info-popularity",
-            text: "Popularity: #" + aninfo.popularityRank
-          }
-        ]
-      })
+            tag: 'h4',
+            className: 'anime-info-popularity',
+            text: `Popularity: #${  aninfo.popularityRank}`,
+          },
+        ],
+      }),
     );
 
-    const imageLoad = document.createElement("img");
-    imageLoad.src = aninfo.coverImage.original;
+    const imageLoad = document.createElement('img');
 
-    imageLoad.addEventListener("load", function(el) {
-      backgroundEl.classList.add("show");
+    /* waits for imageLoad to load, then sets the background */
+    aninfo.coverImage ? imageLoad.src = aninfo.coverImage.original : imageLoad.src =  aninfo.posterImage.original;
+
+    if(aninfo.coverImage){
+    imageLoad.addEventListener('load', function (element) {
+      el.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9) ), url(${aninfo.coverImage.original})`;
       delete this;
-    });
-  }
+    });  
+    }
+    else{
+      el.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9) ), url(${aninfo.posterImage.original})`;
+    }
+  },
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   App.init();
 });
--- helper.js
+++ helper.js
@@ -1,8 +1,36 @@
 window.Helper = {
-    toQueryString: function (params) {
-        let esc = encodeURIComponent;
-        return Object.keys(params)
-        .map(k => esc(k) + '=' + esc(params[k]))
-        .join('&');
+	toQueryString: function (params) {
+		let esc = encodeURIComponent;
+		return Object.keys(params)
+			.map(k => esc(k) + "=" + esc(params[k]))
+			.join("&");
     },
+    
+    crel: (params) => {
+        let el,
+          a;
+        el = document.createElement(params.tag);
+    
+        params.className ? (el.className = params.className) : null;
+    
+        if (params.attributes) {
+          for (a in params.attributes) {
+            el.setAttribute(a, params.attributes[a]);
+          }
+        }
+    
+        params.text && el.appendChild(document.createTextNode(params.text));
+    
+        if (params.children && params.children.length) {
+          for (i = 0; i < params.children.length; i++) {
+            el.appendChild(
+              params.children[i] instanceof window.HTMLElement
+                ? params.children[i]
+                : Helper.crel(params.children[i]),
+            );
+          }
+        }
+    
+        return el;
+      },
 };
